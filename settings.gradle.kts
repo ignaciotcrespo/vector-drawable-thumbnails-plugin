42840febe219f3716807338a66d3da0cd58bda49
Ignacio Tomas Crespo <itcrespo@gmail.com>
2021-04-09 10:49:25 -0300

    merge pull wtf ???

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "vector-drawable-thumbnails-plugin"
--- src/main/kotlin/com/github/ignaciotcrespo/vectordrawablethumbnailsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ignaciotcrespo/vectordrawablethumbnailsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ignaciotcrespo.vectordrawablethumbnailsplugin.services
 
+import com.github.ignaciotcrespo.vectordrawablethumbnailsplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
