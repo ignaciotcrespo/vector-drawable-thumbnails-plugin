87432cdbaf9dbc0e7a3cab4b974c29d61c4d9b6f
GitHub Action <action@github.com>
2021-04-09 12:56:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ignaciotcrespo/vectordrawablethumbnailsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ignaciotcrespo.vectordrawablethumbnailsplugin.services
+
+import com.github.ignaciotcrespo.vectordrawablethumbnailsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
